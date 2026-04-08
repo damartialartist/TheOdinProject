99b0f059ca0672c4110ed8989c5d082d1e68619b
damartialartist <jackychenglhf@gmail.com>
2026-04-08 11:52:26 -0700

    Implemented basic gui and button recognition

--- rock-paper-scissor-gui/script.js
+++ rock-paper-scissor-gui/script.js
@@ -11,13 +11,6 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let userChoice = prompt("Input rock/paper/scissor")
-    return userChoice.toLowerCase();
-}
-
-
-
 
 function playGame() {
 
@@ -25,10 +18,6 @@ function playGame() {
     let computerScore = 0;
     
     function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) {
-            console.log(`You: ${humanChoice}, CPU: ${computerChoice}, Tie`)
-            return;
-        }
         if (humanChoice === "paper") {
             (computerChoice === "scissor") ? computerScore++ : humanScore++;
         } else if (humanChoice === "rock") {
@@ -36,25 +25,16 @@ function playGame() {
         } else if (humanChoice === "scissor") {
             (computerChoice === "rock") ? computerScore++ : humanScore++; 
         }
-        
         console.log(`You: ${humanChoice}, CPU: ${computerChoice}`)
         console.log(`Current Score: You: ${humanScore}, CPU: ${computerScore}`);
     }
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerChoice = getComputerChoice();
 
-        playRound(humanSelection, computerChoice);
-    }
-
-    if (humanScore > computerScore) {
-        console.log("You win!")
-    } else if (humanScore < computerScore) {
-        console.log("You Lose")
-    } else {
-        console.log("Tie")
-    }
+        
 
 }
 
-playGame();
+const humanChoiceButtons = document.getElementById("button-container")
+
+humanChoiceButtons.addEventListener('click', (e) => {
+    const choice = e.target.id;
+});
